8f4d3948d88558372cbd405bf902fae216183e07
SmileLionLY <1252451603@qq.com>
2018-07-22 22:53:17 +0800

    add merge sort

--- Sort/MergeSort/MergeSort.cpp
+++ Sort/MergeSort/MergeSort.cpp
@@ -6,19 +6,23 @@ void Combination(T *array_a, int size_a, T *array_b, int size_b, T *array, int s
     while (index_a < size_a && index_b < size_b) {
         if (array_a[index_a] <= array_b[index_b]) {
             array[index] = array_a[index_a];
+            index_a++;
         } else {
             array[index] = array_b[index_b];
+            index_b++;
         }
         index++;
     }
     if (index_a < size_a) {
         for (int i = 0; i < (size_a - index_a); i++) {
             array[index] = array_a[index_a + i];
+            index++;
         }
     }
     if (index_b < size_b) {
         for (int i = 0; i < (size_b - index_b); i++) {
             array[index] = array_b[index_b + i];
+            index++;
         }
     }
 
@@ -38,13 +42,14 @@ void MergeSort(T *array, int size, int left, int right) {
         Combination(array, middle + 1, array + middle + 1, right - middle, temp, buffer_size);
     }
     for (int i = 0; i < size; i++) {
+        array[i] = temp[i];
         std::cout << temp[i] << " ";
     }
     std::cout << ";" << std::endl;
 }
 
 int main() {
-    int array[5] = {2, 1, 5, 5, 4};
-    MergeSort(array, 5, 0, sizeof(array) / sizeof(4) - 1);
+    int array[3] = {2, 1, 5};
+    MergeSort(array, 3, 0, 2);
     return 0;
 }
